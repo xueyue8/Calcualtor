080eba5cb95cd77695b7656e7805fa93613692fc
Ibokan <ibokan@10-15.local>
2014-03-14 09:29:49 +0800

    Initial Commit

---
+++ Calcualtor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calcualtor
+//
+//  Created by Ibokan on 14-3-14.
+//  Copyright (c) 2014年 gg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
